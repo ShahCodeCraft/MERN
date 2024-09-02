7f8bfa6ade9b59a12ad5eecadf2858224b16d94d
ShahCodeCraft <ershahalam22@gmail.com>
2024-09-02 17:41:42 +0530

    Stripe Payment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import CategoryList from '../components/CategoryList'
-// import BannerProduct from '../components/BannerProduct'
-// import HorizontalCardProduct from '../components/HorizontalCardProduct'
-// import VerticalCardProduct from '../components/VerticalCardProduct'
-
-// function Home() {
-//   return (
-//     <div>
-//      <CategoryList/>
-//      <BannerProduct/>
-
-//      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
-//      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
-
-//      <VerticalCardProduct category={"mobiles"} heading={"Popular's Mobiles"}/>
-//      <VerticalCardProduct category={"Mouse"} heading={"Popular's Mouse"}/>
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
-
-
 import React from 'react'
 import CategoryList from '../components/CategoryList'
 import BannerProduct from '../components/BannerProduct'
