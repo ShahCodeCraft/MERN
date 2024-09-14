f066a92d9994f0cb5835416e0e59a3e7d02729cb
ShahCodeCraft <ershahalam22@gmail.com>
2024-09-14 12:55:52 +0530

    vertical card changes

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,8 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    // origin : process.env.FRONTEND_URL,
+    origin :"http://localhost:5173",
     credentials : true
 }))
 
